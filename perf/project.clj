89767d7106d6419a7f4e07acd8991717fc8cc604
Curtis Summers <curtis.summers@layerware.com>
2015-07-15 09:27:25 -0500

    version 0.2.0

--- perf/project.clj
+++ perf/project.clj
@@ -1,4 +1,4 @@
-(defproject pgqueue-perf "0.1.0-SNAPSHOT"
+(defproject pgqueue-perf "0.2.0-SNAPSHOT"
   :description "pgqueue perf"
   :url "https://github.com/layerware/pgqueue/perf"
   :dependencies [[org.clojure/clojure "1.7.0"]
