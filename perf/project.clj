8acf4ba19b9b300d366978422e66435fe731c2dd
Curtis Summers <curtis.summers@layerware.com>
2015-07-15 14:17:49 -0500

    replace naive perf test

--- perf/project.clj
+++ perf/project.clj
@@ -2,5 +2,7 @@
   :description "pgqueue perf"
   :url "https://github.com/layerware/pgqueue/perf"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.layerware/pgqueue "0.2.0-SNAPSHOT"]]
-  :jvm-opts ^:replace ["-Xmx1g" "-Xms1g" "-server" ])
+                 [com.layerware/pgqueue "0.2.1"]]
+  :jvm-opts ^:replace ["-Xmx1g" "-Xms1g" "-server"]
+  :global-vars {*warn-on-reflection* false
+                *assert* true})
--- perf/src/pgqueue/perf.clj
+++ perf/src/pgqueue/perf.clj
@@ -9,17 +9,32 @@
 (defn now-diff [start]
   (- (System/currentTimeMillis) start))
 
+(defn avg [n tm]
+  (format "%7.3f/ms" (/ (float n) tm)))
+
+(defn print-timings [n start]
+  (let [diff (now-diff start)]
+      (println (ms diff) "duration" (avg n diff) "avg rate")))
+
 (defn int-run  [q n]
-  (let [start (System/currentTimeMillis)]
-    (print (format "Put  %7d integers..." n))
-    (let [pool (map (fn [i] (future (pgq/put q i))) (range n))]
-      (doall (map deref pool)))
-    (println (ms (now-diff start)))
+  (let [workers 32
+        start (System/currentTimeMillis)]
+
+    (print (format "\nPut  %7d integers..." n))
+    (doall (pmap #(pgq/put q %) (range n)))
+    (print-timings n start)
+    
 
     (print (format "Take %7d integers..." n))
-    (let [pool (map (fn [i] (future (pgq/take q))) (range n))]
-      (doall (map deref pool)))
-    (println (ms (now-diff start)))))
+    (let [work (repeat workers
+                 (future
+                   (doall (take-while #(not (nil? %))
+                            (repeatedly #(pgq/take q))))))]
+      (doall (map deref work)))
+    (print-timings n start)
+
+    ; make sure we actually took!
+    (assert (= 0 (pgq/count q)))))
 
 (defn -main []
   (let [p (System/getenv "PGQUEUE_CONFIG")
@@ -27,7 +42,9 @@
         _ (pgq/destroy-all-queues! c)
         q (pgq/queue :perf c)]
 
+    (println "pgqueue perf test")
     (int-run q 100)
     (int-run q 1000)
-    (int-run q 10000))
+    (int-run q 10000)
+    )
   (System/exit 1))
