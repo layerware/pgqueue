d2fc63cdb7abc6f9cfaa6f3deab5c2a17c020a85
Curtis Summers <curtis.summers@layerware.com>
2015-07-18 08:56:32 -0500

    version 0.3.1

--- perf/project.clj
+++ perf/project.clj
@@ -1,8 +1,8 @@
-(defproject pgqueue-perf "0.3.0-SNAPSHOT"
+(defproject pgqueue-perf "0.3.1-SNAPSHOT"
   :description "pgqueue perf"
   :url "https://github.com/layerware/pgqueue/perf"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.layerware/pgqueue "0.3.0"]]
+                 [com.layerware/pgqueue "0.3.1"]]
   :jvm-opts ^:replace ["-Xmx1g" "-Xms1g" "-server"]
   :global-vars {*warn-on-reflection* false
                 *assert* true})
